3161385168771a4f26b454f2be506ee09487099a
ViktorGoloviznin <viktor.goloviznin@gmail.com>
2024-05-08 10:24:17 +0200

    install tailwind

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,26 +4,34 @@ import Dropdown from './Dropdown';
 import { useState } from 'react';
 import LoginForm from './LoginForm';
 
+// Объявляем компонент Header
 export default function Header() {
-
+  // Состояние для отображения/скрытия модального окна входа
   const [showLoginModal, setShowLoginModal] = useState(false);
   const toggleLoginModal = () => {
     setShowLoginModal(!showLoginModal);
   };
 
+  // Возвращаем JSX для отображения заголовка и навигации
   return (
     <header>
+      {/* Навигационное меню */}
       <nav className="header-nav">
+        {/* Логотип */}
         <NavLink to="/" className="logo">
           <img src={logo} alt="Home" />
         </NavLink>
+        {/* Список ссылок для навигации */}
         <ul className="nav-links">
           
+          {/* Элемент навигации с выпадающим меню */}
           <li className="drop">
+            {/* Компонент Dropdown, который реализует выпадающее меню */}
             <NavLink to="pools" style={{ textDecoration: 'none' }}>
               <Dropdown />
             </NavLink>
           </li>
+          {/* Остальные элементы навигации */}
           <li>
             <NavLink to="hamams" style={{ textDecoration: 'none' }}>
               Хамамы
@@ -45,16 +53,21 @@ export default function Header() {
             </NavLink>
           </li>
 
+          {/* Кнопка для открытия модального окна входа */}
           <button onClick={toggleLoginModal} className="login_btn">
             Вход
           </button>
+          {/* Модальное окно для входа */}
           {showLoginModal && (
             <div className="login-modal">
               <div className="login-modal-content">
+                {/* Иконка закрытия модального окна */}
                 <span className="close-icon" onClick={toggleLoginModal}>
                   &times;
                 </span>
+                {/* Компонент LoginForm для ввода данных пользователя */}
                 <LoginForm />
+                {/* Ссылка на страницу регистрации */}
                 <NavLink to="/register" style={{ textDecoration: 'none' }}>
                   Регистрация
                 </NavLink>
@@ -67,4 +80,3 @@ export default function Header() {
     </header>
   );
 };
-
--- src/components/Header/LoginForm.tsx
+++ src/components/Header/LoginForm.tsx
@@ -1,15 +1,20 @@
 import React, { useState } from 'react';
 
+// Объявление функционального компонента LoginForm
 const LoginForm = () => {
+    // Состояния для email и password, используемые для управления данными в форме
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    // Обработчик события (предотвращает перезагрузку страницы) 
     const handleSubmit = (event: { preventDefault: () => void; }) => {
         event.preventDefault();
     };
 
+    // Возвращение JSX для отображения формы входа
     return (
         <form onSubmit={handleSubmit}>
+            {/* Поле ввода email */}
             <label htmlFor="email">Email:</label>
             <br />
             <input
@@ -17,9 +22,10 @@ const LoginForm = () => {
                 id="email"
                 value={email}
                 onChange={(event) => setEmail(event.target.value)}
-                required
+                required // Поле обязательно для заполнения
             />
             <br />
+            {/* Поле для ввода пароля */}
             <label htmlFor="password">Пароль:</label>
             <br />
             <input
@@ -27,12 +33,13 @@ const LoginForm = () => {
                 id="password"
                 value={password}
                 onChange={(event) => setPassword(event.target.value)}
-                required
+                required // Поле обязательно для заполнения
             />
             <br />
+            {/* Кнопка для отправки формы */}
             <button type="submit">Войти</button>
         </form>
     );
 };
 
-export default LoginForm; 
+export default LoginForm;
